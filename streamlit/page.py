f0e2f65a3b0c9edb45d42dc9aecbac6168db45e9
legolego <oleg_gitgub@olegs.com>
2023-05-19 11:56:46 -0400

    initial commit

---
+++ streamlit/page.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from pathlib import Path
+import streamlit as st
+
+
+def getPathInStreamlitDir():
+    return Path(__file__).parents[1] / str('streamlit/')
+
+
+st.title('This is a test of Streamlit cloud from Deepnote through Github')
+
+
+st.markdown("## This is a test of Streamlit cloud from Deepnote through Github")
+
+img_name = 'smile.jpg'
+img_path = getPathInStreamlitDir() / str('assets/' + img_name)
+image = Image.open(img_path)
+
+
+st.image(image)
