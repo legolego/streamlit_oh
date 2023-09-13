76ed9b490377d5321ead999674247cd0453bcda9
Oleg Nikolsky <nikolsky@umich.edu>
2023-09-13 04:52:23 +0000

    init commit

---
+++ streamlit/demo.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+from pathlib import Path
+
+def getPathInStreamlitDir():
+    return Path(__file__).parents[1] / str('streamlit/')
+
+
+st.title('This is a test of Streamlit cloud from Deepnote through Github')
+
+st.markdown('Hello World!!!??!!??**')
+
+img_name = 'smile.jpg'
+img_path = getPathInStreamlitDir() / str('assets/' + img_name)
+image = Image.open(img_path)
+
+
+st.image(image)
