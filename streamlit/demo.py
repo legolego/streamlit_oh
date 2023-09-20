43813f66f67e72628049c142708ee09a40d745b1
Oleg Nikolsky <nikolsky@umich.edu>
2023-09-20 13:44:06 +0000

    added iris data csv

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from PIL import Image
 from pathlib import Path
+import pandas as pd
+import altair as alt
+
 
 def getPathInStreamlitDir():
     return Path(__file__).parents[1] / str('streamlit/')
@@ -16,3 +19,23 @@ image = Image.open(img_path)
 
 
 st.image(image)
+
+st.markdown('The dataframe below is from a csv file that lives in `streamlit/data`.')
+
+csv_path = getPathInStreamlitDir() / str('data/IRIS.csv')
+
+df_iris = pd.read_csv(csv_path)
+
+st.dataframe(df_iris)
+
+st.markdown("Now let's make an Altair plot for another example.")
+
+plot = alt.Chart(df_iris).mark_point().encode(
+  # Map the sepalLength to x-axis
+    x = 'sepalLength',
+  # Map the petalLength to y-axis
+    y = 'petalLength',
+    color = 'variety'
+)
+
+st.altair_chart(plot)
